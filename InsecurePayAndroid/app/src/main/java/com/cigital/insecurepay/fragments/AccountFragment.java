5df91382390ded40204865f7b151f6f9e996f420
Jaini Vora <voraj@iu.edu>
2016-02-21 18:15:06 -0500

    Basic UI Change Password

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/AccountFragment.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/AccountFragment.java
@@ -1,10 +1,14 @@
 package com.cigital.insecurepay.fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cigital.insecurepay.R;
 
@@ -31,6 +36,7 @@ public class AccountFragment extends Fragment {
     EditText etAddress;
     Button btnUpdateInfo;
     Button btnChangePassword;
+
 
     private OnFragmentInteractionListener mListener;
     private TextWatcher twEmail = new TextWatcher() {
@@ -149,6 +155,8 @@ public class AccountFragment extends Fragment {
 
     private void onClickChangePassword() {
         Log.i(this.getClass().getSimpleName(), "Displaying change password dialog");
+        changePassword();
+        //return true;
     }
 
     private void initValues() {
@@ -165,5 +173,80 @@ public class AccountFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
+    private void changePassword() {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this.getContext());
+        View dialogView = layoutInflater.inflate(R.layout.dialog_change_password, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this.getContext());
+        alertDialogBuilder.setView(dialogView);
+        alertDialogBuilder.setPositiveButton("OK", null);
+        alertDialogBuilder.setNegativeButton("Cancel", null);
+
+        final EditText password1View;
+        final EditText password2View;
+
+        // EditText variables to fetch user inputs from the dialog
+        password1View = (EditText) dialogView.findViewById(R.id.et_password_field1);
+        password2View = (EditText) dialogView.findViewById(R.id.et_password_field2);
+
+
+        final AlertDialog alertD = alertDialogBuilder.create();
+
+        alertD.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                alertD.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String password_field1 = password1View.getText().toString();
+                        String password_field2 = password2View.getText().toString();
+                        //ChangePasswordTask changePasswordTask;
+                        boolean cancel = false;
+                        View focusView = null;
+                        Log.d("Password 1 ", password_field1);
+                        Log.d("Password 2 ", password_field2);
+
+                        if (TextUtils.isEmpty(password_field1)) {
+                            password1View.setError(getString(R.string.error_field_required));
+                            return;
+                        }
+
+                        if ((password_field1.length() < 3) || (password_field2.length() < 3)) {
+                            password2View.setError("Minimum 3 characters length required");
+                            password1View.setText("");
+                            password2View.setText("");
+                            return;
+                        }
+
+                        if (TextUtils.isEmpty(password_field2)) {
+                            password2View.setError(getString(R.string.error_invalid_field));
+                            return;
+                        }
+
+                        if (password_field1.equals(password_field2)) {
+                           // changePasswordTask = new ChangePasswordTask("foo",password_field1);
+                           // changePasswordTask.execute("foo", password_field1);
+                            Toast.makeText(AccountFragment.this.getContext(), "Password match", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            password1View.setText("");
+                            password2View.setText("");
+                            Toast.makeText(AccountFragment.this.getContext(), "Password mismatch", Toast.LENGTH_LONG).show();
+                        }
+
+                        alertD.dismiss();
+                    }
+                });
+                alertD.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertD.dismiss();
+                    }
+                });
+            }
+        });
+
+        alertD.show();
+    }
 
 }
