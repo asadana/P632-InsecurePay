8cac7c478b8b2bc276741ae5662f8a048d91dacc
Ankit Sadana <asadana@indiana.edu>
2016-02-21 09:36:39 -0500

    #30. Added textWatcher and listeners

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/AccountFragment.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/AccountFragment.java
@@ -3,9 +3,16 @@ package com.cigital.insecurepay.fragments;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.cigital.insecurepay.R;
 
@@ -15,7 +22,68 @@ import com.cigital.insecurepay.R;
  */
 public class AccountFragment extends Fragment {
 
+    TextView tvName;
+    TextView tvAccountNumber;
+    TextView tvSSN;
+    TextView tvUserDOB;
+    EditText etEmail;
+    EditText etPhone;
+    EditText etAddress;
+    Button btnUpdateInfo;
+    Button btnChangePassword;
+
     private OnFragmentInteractionListener mListener;
+    private TextWatcher twEmail = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            btnUpdateInfo.setEnabled(true);
+            Log.i(this.getClass().getSimpleName(), "Email Address value changed.");
+        }
+    };
+    private TextWatcher twAddress = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            btnUpdateInfo.setEnabled(true);
+            Log.i(this.getClass().getSimpleName(), "Address value changed.");
+        }
+    };
+    private TextWatcher twPhone = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            btnUpdateInfo.setEnabled(true);
+            Log.i(this.getClass().getSimpleName(), "Phone number value changed.");
+        }
+    };
 
     public AccountFragment() {
         // Required empty public constructor
@@ -34,11 +102,68 @@ public class AccountFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View viewObj = inflater.inflate(R.layout.fragment_account, container, false);
+
+        // Initializing all objects from fragment_account
+        tvName = (TextView) viewObj.findViewById(R.id.tvAccount_fillName);
+        tvAccountNumber = (TextView) viewObj.findViewById(R.id.tvAccount_fillAccountNumber);
+        tvSSN = (TextView) viewObj.findViewById(R.id.tvAccount_fillSSN);
+        tvUserDOB = (TextView) viewObj.findViewById(R.id.tvAccount_fillUserDOB);
+
+        etEmail = (EditText) viewObj.findViewById(R.id.etAccount_fillEmail);
+        etPhone = (EditText) viewObj.findViewById(R.id.etAccount_fillPhone);
+        etAddress = (EditText) viewObj.findViewById(R.id.etAccount_fillAddress);
+
+        btnUpdateInfo = (Button) viewObj.findViewById(R.id.btnAccount_update);
+        btnChangePassword = (Button) viewObj.findViewById(R.id.btnAccount_changePassword);
+
+        initValues();
+        addListeners();
+
         return viewObj;
+    }
+
+    private void addListeners() {
+        etEmail.addTextChangedListener(twEmail);
+        etAddress.addTextChangedListener(twAddress);
+        etPhone.addTextChangedListener(twPhone);
+        twPhone = new PhoneNumberFormattingTextWatcher();
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickUpdateInformation();
+            }
+        });
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickChangePassword();
+            }
+        });
+    }
+
+    private void onClickUpdateInformation() {
+        Log.i(this.getClass().getSimpleName(), "Updating customer information.");
+    }
+
+    private void onClickChangePassword() {
+        Log.i(this.getClass().getSimpleName(), "Displaying change password dialog");
+    }
+
+    private void initValues() {
+        // TODO: Update with call to server
+        etPhone.setText("0000000000", TextView.BufferType.EDITABLE);
+        etAddress.setText("I dont live here", TextView.BufferType.EDITABLE);
+        etEmail.setText("something@gmail.com", TextView.BufferType.EDITABLE);
+
+
     }
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
 }
