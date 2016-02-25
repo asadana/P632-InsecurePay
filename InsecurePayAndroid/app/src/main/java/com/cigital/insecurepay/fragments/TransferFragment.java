6a987dc2b6e686315f96282d95105f978a6765fe
madhradh <madhradh@iu.edu>
2016-02-24 22:29:22 -0500

    Created VO and basic UI (needs more stuff in the UI)

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/TransferFragment.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/TransferFragment.java
@@ -2,77 +2,53 @@ package com.cigital.insecurepay.fragments;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cigital.insecurepay.R;
+import com.cigital.insecurepay.VOs.AccountVO;
+import com.google.gson.Gson;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link TransferFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link TransferFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TransferFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private TransferFetchTask task = null;
+    private Gson gson = new Gson();
+    EditText etAccountNumber;
+    EditText etTransferAmount;
+    Button bTransfer;
     private OnFragmentInteractionListener mListener;
 
     public TransferFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TransferFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TransferFragment newInstance(String param1, String param2) {
-        TransferFragment fragment = new TransferFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transfer, container, false);
+        View viewObj = inflater.inflate(R.layout.fragment_home, container, false);
+        etAccountNumber = (EditText)viewObj.findViewById(R.id.etAccountNumber);
+        etTransferAmount = (EditText)viewObj.findViewById(R.id.etTransferAmount);
+        bTransfer = (Button)viewObj.findViewById(R.id.bTransfer);
+        onButtonPressed(bTransfer);
+        return viewObj;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void onButtonPressed(Button uri) {
+        task = new TransferFetchTask();
+        task.execute();
     }
 
     @Override
@@ -105,5 +81,13 @@ public class TransferFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+    class TransferFetchTask extends AsyncTask<String, String, AccountVO> {
+
+
+        @Override
+        protected AccountVO doInBackground(String... params) {
+            return null;
+        }
     }
 }
