fa35d706fd919fa7338c879e1b099ec5ffc7e9be
Ankit Sadana <asadana@indiana.edu>
2016-03-25 16:21:41 -0400

    Returning true on logout

--- server/src/com/application/service/LogoutService.java
+++ server/src/com/application/service/LogoutService.java
@@ -1,5 +1,6 @@
 package com.application.service;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -8,10 +9,13 @@ import com.application.common.Constants;
 @Path("/logout")
 public class LogoutService extends BaseService{
 
-	public void Logout()
+	@POST
+	public Response Logout()
 	{
 		Constants.cookieList.deleteCookies();
 		Logging.logger.debug("Cookies after deletion : " + Constants.cookieList.displayCookies());
+		
+		return Response.status(Response.Status.ACCEPTED).entity(true).build();
 	}
 
 }
