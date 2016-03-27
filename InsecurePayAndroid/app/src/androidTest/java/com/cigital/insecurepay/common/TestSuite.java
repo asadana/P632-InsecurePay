78e82467bf1d9c60f2a2aeb7f1cbe7dfbeb54a36
Ankit Sadana <asadana@indiana.edu>
2016-03-27 13:21:33 -0400

    #70 Created a basic test suite to integrate tests

---
+++ InsecurePayAndroid/app/src/androidTest/java/com/cigital/insecurepay/common/TestSuite.java
@@ -0,0 +1,24 @@
+package com.cigital.insecurepay.common;
+
+import com.cigital.insecurepay.activity.ForgotPasswordTest;
+import com.cigital.insecurepay.activity.HomePageTest;
+import com.cigital.insecurepay.activity.LoginActivityTest;
+import com.cigital.insecurepay.activity.LogoutTest;
+import com.cigital.insecurepay.fragments.AccountFragment;
+import com.cigital.insecurepay.fragments.ActivityHistoryFragmentTest;
+import com.cigital.insecurepay.fragments.TransferFragmentTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ForgotPasswordTest.class,
+        LoginActivityTest.class,
+        HomePageTest.class,
+        AccountFragment.class,
+        TransferFragmentTest.class,
+        ActivityHistoryFragmentTest.class,
+        LogoutTest.class
+})
+public class TestSuite {}
