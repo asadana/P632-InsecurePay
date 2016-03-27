0bb63229b9381472f2e8f081c9e3cab60500f975
Amish Shah <shah7@indiana.edu>
2016-03-27 17:35:35 -0400

    Add/Deleted logs, displayed interest in text view  interest calculator #85

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/InterestCalcFragment.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/InterestCalcFragment.java
@@ -42,6 +42,8 @@ public class InterestCalcFragment extends Fragment {
     private TextView tvDisplayCreditScore;
     private Button btnCalculate;
     private Spinner DateType;
+    private TextView tvInterest;
+    private TextView tvFillInterest;
     private CommonVO commonVO;
 
 
@@ -53,32 +55,13 @@ public class InterestCalcFragment extends Fragment {
     private static final int BAD = 300;
     //Base interest rate
     private static final int INTERESTBASE = 10;
-    // Number of Months
-    private static final int MONTHS =12;
-    //Number of Days
-    private static final int DAYS =365;
+    private static final int MONTHS = 12;
+    private static final int DAYS = 365;
+    private static final int ROUNDOFFVALUE = 2;
 
     private Integer creditScore;
     private Double interest;
     private HomeFragment.OnFragmentInteractionListener mListener;
-
-    // TextWatcher objects to handle on edit events for EditText fields
-    private TextWatcher twDateEntry = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            btnCalculate.setEnabled(true);
-            Log.i(this.getClass().getSimpleName(), "Date entered");
-        }
-    };
-
 
 
     public InterestCalcFragment() {
@@ -99,8 +82,9 @@ public class InterestCalcFragment extends Fragment {
         //Log.d(this.getClass().getSimpleName(), "The interest is " + );
         return viewObj;
     }
+
     private int generateCreditScore() {
-        Log.i(this.getClass().getSimpleName(),"Generating credit Score ... ");
+        Log.i(this.getClass().getSimpleName(), "Generating credit Score ... ");
         commonVO = ((CommonVO) this.getArguments().getSerializable(getString(R.string.common_VO)));
         double account_bal = commonVO.getAccountVO().getAccountBalance();
         int creditScoreRank = 1;
@@ -112,44 +96,38 @@ public class InterestCalcFragment extends Fragment {
             creditScoreRank = 2;
         }
 
-        Random r = new Random((long)account_bal);
+        Random r = new Random((long) account_bal);
         int creditScore = 0;
-        Log.d(this.getClass().getSimpleName(), "Credit Score Rank" + creditScoreRank);
-        switch(creditScoreRank)
-        {
+        switch (creditScoreRank) {
             //Excellent
-            case 1:
-            {
-                creditScore = r.nextInt(MAXSCORE-EXCELLENT) + EXCELLENT;
+            case 1: {
+                creditScore = r.nextInt(MAXSCORE - EXCELLENT) + EXCELLENT;
                 break;
             }
             //Good
-            case 2:
-            {
-                creditScore = r.nextInt(EXCELLENT-GOOD)+GOOD;
+            case 2: {
+                creditScore = r.nextInt(EXCELLENT - GOOD) + GOOD;
                 break;
             }
             //Fair
-            case 3:
-            {
-                creditScore = r.nextInt(GOOD-FAIR)+FAIR;
+            case 3: {
+                creditScore = r.nextInt(GOOD - FAIR) + FAIR;
                 break;
             }
             //Bad
-            case 4:
-            {
-                creditScore = r.nextInt(FAIR-BAD)+BAD;
+            case 4: {
+                creditScore = r.nextInt(FAIR - BAD) + BAD;
                 break;
             }
         }
-        Log.d(this.getClass().getSimpleName(), "Credit Score" + creditScore);
+
         return creditScore;
     }
 
     // Initializing listeners where needed
-    private void addListener(){
+    private void addListener() {
         Log.i(this.getClass().getSimpleName(), "Adding Listeners");
-        etDate.addTextChangedListener(twDateEntry);
+
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -157,23 +135,23 @@ public class InterestCalcFragment extends Fragment {
             }
         });
     }
+
     private void initValues(View viewObj) {
         Log.i(this.getClass().getSimpleName(), "Initializing Values..");
-
-        commonVO = ((CommonVO) this.getArguments().getSerializable(getString(R.string.common_VO)));
         tvBalance = (TextView) viewObj.findViewById(R.id.tvIntCalc_AccountBalance);
-        tvDisplayBalance= (TextView) viewObj.findViewById(R.id.tvIntCalc_FillAccountBalance);
+        tvDisplayBalance = (TextView) viewObj.findViewById(R.id.tvIntCalc_FillAccountBalance);
         tvDisplayBalance.setText(String.valueOf(commonVO.getAccountVO().getAccountBalance()));
         tvPrincipal = (TextView) viewObj.findViewById(R.id.tvIntCalc_PrincipalAmount);
         etPrincipal = (EditText) viewObj.findViewById(R.id.etIntCalc_FillPrincipalAmount);
         etPrincipal.setText(String.valueOf(commonVO.getAccountVO().getAccountBalance()));
         etDate = (EditText) viewObj.findViewById(R.id.etIntCalc_Date);
-        DateType=(Spinner) viewObj.findViewById(R.id.etIntCalc_Period);
+        DateType = (Spinner) viewObj.findViewById(R.id.etIntCalc_Period);
         tvPeriod = (TextView) viewObj.findViewById(R.id.tvIntCalc_Period);
-        tvCreditScore =  (TextView) viewObj.findViewById(R.id.tvIntCalc_CreditScore);
+        tvCreditScore = (TextView) viewObj.findViewById(R.id.tvIntCalc_CreditScore);
         tvDisplayCreditScore = (TextView) viewObj.findViewById(R.id.tvIntCalc_FillCreditScore);
-
-        btnCalculate=(Button) viewObj.findViewById(R.id.btnIntCalc_Calc);
+        tvInterest = (TextView) viewObj.findViewById(R.id.tvIntCalc_Interest);
+        tvFillInterest = (TextView) viewObj.findViewById(R.id.tvIntCalc_FillInterest);
+        btnCalculate = (Button) viewObj.findViewById(R.id.btnIntCalc_Calc);
         Spinner spinner = (Spinner) viewObj.findViewById(R.id.etIntCalc_Period);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(),
                 R.array.Date, android.R.layout.simple_spinner_item);
@@ -181,6 +159,7 @@ public class InterestCalcFragment extends Fragment {
         spinner.setAdapter(adapter);
 
     }
+
     private void calcInterest() {
         Log.i(this.getClass().getSimpleName(), "Calculating Interest...");
         double principal = Double.parseDouble(etPrincipal.getText().toString());
@@ -188,34 +167,36 @@ public class InterestCalcFragment extends Fragment {
 
         String type = DateType.getSelectedItem().toString();
         //If month or days convert to year
-        switch(type){
+        switch (type) {
             case "Months":
-                time /=MONTHS;
+                time /= MONTHS;
                 break;
             case "Days":
-                time /=DAYS;
+                time /= DAYS;
                 break;
         }
 
         //Calc rate of interest
-        commonVO = ((CommonVO) this.getArguments().getSerializable(getString(R.string.common_VO)));
         double amount = commonVO.getAccountVO().getAccountBalance();
         int creditScore = Integer.parseInt(tvDisplayCreditScore.getText().toString());
-        Log.d(this.getClass().getSimpleName(), "Credit Score" + creditScore);
         //Adding (1-creditscore/1000) to the base interest rate
-        Double rateOfInterest= INTERESTBASE + (1-(creditScore/1000.0));
+        Double rateOfInterest = INTERESTBASE + (1 - (creditScore / 1000.0));
 
         //Round off to 2 decimal places after point
 
         rateOfInterest = roundOff(rateOfInterest);
-        interest = (rateOfInterest * principal * time) /100.0;
+        interest = (rateOfInterest * principal * time) / 100.0;
         interest = roundOff(interest);
+        tvInterest.setVisibility(View.VISIBLE);
+        tvFillInterest.setText(interest.toString());
     }
 
-    private Double roundOff(double value){
-        Double temp = new BigDecimal(value)
-                .setScale(2, BigDecimal.ROUND_HALF_UP)
+    //Round off to 2 decimal places after point
+    private Double roundOff(double value) {
+        Log.i(this.getClass().getSimpleName(), "Rounding Off...");
+        return new BigDecimal(value)
+                .setScale(ROUNDOFFVALUE, BigDecimal.ROUND_HALF_UP)
                 .doubleValue();
-        return temp;
+
     }
 }
