9198c808c76cd3c60c57efef02e5cd2dcca49661
Ankit Sadana <asadana@indiana.edu>
2016-03-31 18:01:06 -0400

    #70 Removed hardcoded values from ActivityHistory

--- InsecurePayAndroid/app/src/androidTest/java/com/cigital/insecurepay/fragments/ActivityHistoryFragmentTest.java
+++ InsecurePayAndroid/app/src/androidTest/java/com/cigital/insecurepay/fragments/ActivityHistoryFragmentTest.java
@@ -6,16 +6,16 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.cigital.insecurepay.R;
 import com.cigital.insecurepay.activity.LoginActivity;
+import com.cigital.insecurepay.common.Constants;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -27,17 +27,13 @@ public class ActivityHistoryFragmentTest {
     @Rule
     public final ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
-    public static final String incorrectAccNumber = "2000";
-    public static final String correctAccNumber = "2005";
-    public static final String correctPassword = "abcde";
-    public static final String correctUsername = "foo";
-
     @Test
     public void checkRetrieval() {
         onView(withId(R.id.username)).
-                perform(typeText(correctUsername), closeSoftKeyboard());
+                perform(replaceText(Constants.correctUsername), closeSoftKeyboard());
         onView(withId(R.id.password)).
-                perform(typeText(correctPassword), closeSoftKeyboard());
+                perform(replaceText(Constants.correctPassword), closeSoftKeyboard());
+
         // First attempt with correct username and password
         onView(withId(R.id.btnSignIn))
                 .perform(click());
@@ -50,11 +46,6 @@ public class ActivityHistoryFragmentTest {
         onView(withText(R.string.nav_activity_history))
                 .perform(click());
 
-        //Check for correct account number
-        onView(withId(R.id.etActivityHistory_AccountNo))
-                .check(matches(withText(correctAccNumber)));
-
-
         onView(withId(R.id.btnSubmit))
                 .perform(click());
 
@@ -64,41 +55,4 @@ public class ActivityHistoryFragmentTest {
                 .check(matches(isDisplayed()));
 
     }
-
-    @Test
-    public void checkHPERetrieval() {
-        onView(withId(R.id.username)).
-                perform(typeText(correctUsername), closeSoftKeyboard());
-        onView(withId(R.id.password)).
-                perform(typeText(correctPassword), closeSoftKeyboard());
-        // First attempt with correct username and password
-        onView(withId(R.id.btnSignIn))
-                .perform(click());
-
-        // Open Drawer
-        onView(withId(R.id.drawer_layout))
-                .perform(DrawerActions.open());
-
-        // Click on Account Management
-        onView(withText(R.string.nav_activity_history))
-                .perform(click());
-
-        //Check for correct account number
-        onView(withId(R.id.etActivityHistory_AccountNo))
-                .check(matches(withText(correctAccNumber)));
-
-        //Perform HPE
-        onView(withId(R.id.etActivityHistory_AccountNo))
-                .perform(clearText(), typeText(incorrectAccNumber), closeSoftKeyboard());
-
-        onView(withId(R.id.btnSubmit))
-                .perform(click());
-
-        //Check whether there is a list
-        onView(withId(R.id.lvActivityHistory_transactionList))
-                .perform(click())
-                .check(matches(isDisplayed()));
-
-    }
-
 }
