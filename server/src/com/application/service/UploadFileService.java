ce6d209214d3fc01ae6f96ed54f02d72289e051e
Ankit Sadana <asadana@indiana.edu>
2016-04-09 09:04:48 -0400

    Updated UploadFileService

--- server/src/com/application/service/UploadFileService.java
+++ server/src/com/application/service/UploadFileService.java
@@ -1,10 +1,8 @@
 package com.application.service;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -15,6 +13,9 @@ import javax.ws.rs.core.Response;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
+import com.application.common.DaoFactory;
+import com.application.dao.FileUploadDao;
+
 @Path("/chatFileUpload")
 public class UploadFileService extends BaseService {
 
@@ -24,38 +25,21 @@ public class UploadFileService extends BaseService {
 			@FormDataParam("file") InputStream uploadedInputStream,
 			@FormDataParam("file") FormDataContentDisposition fileDetail) {
 
-		String uploadedFileLocation = "/usr/share/tomcat8/temp/uploadedFiles/" + fileDetail.getFileName();
-
-		// save it
-		writeToFile(uploadedInputStream, uploadedFileLocation);
-
-		String output = "File uploaded to : " + uploadedFileLocation;
-
-		return Response.status(200).entity(output).build();
-
-	}
-
-	// save uploaded file to new location
-	private void writeToFile(InputStream uploadedInputStream,
-			String uploadedFileLocation) {
-
+		String resultObj = null;
 		try {
-			OutputStream out = new FileOutputStream(new File(
-					uploadedFileLocation));
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			out = new FileOutputStream(new File(uploadedFileLocation));
-			while ((read = uploadedInputStream.read(bytes)) != -1) {
-				out.write(bytes, 0, read);
-			}
-			out.flush();
-			out.close();
-		} catch (IOException e) {
-
-			e.printStackTrace();
+			resultObj = DaoFactory.getInstance(FileUploadDao.class, 
+					this.getConnection()).saveUploadedFile(uploadedInputStream,
+							fileDetail.getFileName());
+		} catch (ClassNotFoundException | InstantiationException 
+				| IllegalAccessException | NoSuchMethodException 
+				| SecurityException | IllegalArgumentException 
+				| InvocationTargetException | SQLException e) {
+			logger.error(e);
+			Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 		}
 
+		return Response.status(Response.Status.ACCEPTED).entity(resultObj).build();
 	}
 
+	
 }
