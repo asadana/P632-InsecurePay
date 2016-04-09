f1bf6ebe1d0d29c4a910556386a1d61db55a6a6b
Ankit Sadana <asadana@indiana.edu>
2016-04-09 18:36:37 -0400

    #89 Added comments to SplashScreenActivity. Started reformatting strings.xml

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/activity/SplashScreenActivity.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/activity/SplashScreenActivity.java
@@ -5,8 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+/**
+ * SplashScreenActivity is an empty activity class with no layout file.
+ * This class is used to display a splash screen to the user
+ * before passing the control to LoginActivity.
+ */
 public class SplashScreenActivity extends AppCompatActivity {
 
+    /**
+     * onCreate is the first method called when the Activity is being created.
+     * It puts the activity UI thread to sleep for 1500 milliseconds while the
+     * splash screen is displayed. Then passes the control to LoginActivity class.
+     *
+     * @param savedInstanceState object that may be used to pass data to while
+     *                           creating this Activity.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
