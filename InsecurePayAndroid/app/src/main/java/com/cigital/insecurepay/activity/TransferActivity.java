055023a333ccb37a67425b052ce08d3c5f074590
Ankit Sadana <asadana@indiana.edu>
2016-04-15 16:32:41 -0400

    #89 Clean of TransferActivity

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/activity/TransferActivity.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/activity/TransferActivity.java
@@ -19,6 +19,10 @@ import com.cigital.insecurepay.VOs.CommonVO;
 import com.cigital.insecurepay.VOs.TransferFundsVO;
 import com.cigital.insecurepay.common.PostAsyncCommonTask;
 
+/**
+ * TransferActivity is an activity class that displays all the transfer information, giving the
+ * user an option to review and confirm or cancel the transaction.
+ */
 public class TransferActivity extends AppCompatActivity {
 
     // View objects
@@ -29,8 +33,16 @@ public class TransferActivity extends AppCompatActivity {
     private TextView tvToCustomerNo;
     private TextView tvTransferDetails;
     private TransferFundsVO transferFundsVO;
+
     private CommonVO commonVO;
 
+    /**
+     * onCreate is the first method called when the Activity is being created.
+     * It populates and initializes the text views.
+     *
+     * @param savedInstanceState Object that is used to pass data to this activity while
+     *                           creating it.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,13 +50,14 @@ public class TransferActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //Setup the Transfer Activity
+        // Setting up UI components
         tvTransferAmount = (TextView) findViewById(R.id.tvTransfer_filledAmount);
         tvFromAccountNo = (TextView) findViewById(R.id.tvTransfer_filledfromAccountNo);
         tvToAccountNo = (TextView) findViewById(R.id.tvTransfer_filledtoAccountNo);
         tvFromCustomerNo = (TextView) findViewById(R.id.tvTransfer_filledfromCustNo);
         tvToCustomerNo = (TextView) findViewById(R.id.tvTransfer_filledtoCustNo);
         tvTransferDetails = (TextView) findViewById(R.id.tvTransfer_filledDetails);
+
         Button btnCancel = (Button) findViewById(R.id.btnCancel);
         Button btnConfirm = (Button) findViewById(R.id.btnConfirm);
 
@@ -56,7 +69,7 @@ public class TransferActivity extends AppCompatActivity {
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("OnClickCancel", "cancel");
+                Log.d(this.getClass().getSimpleName(), "onCreate: Cancel selected.");
                 onBackPressed();
             }
         });
@@ -70,8 +83,12 @@ public class TransferActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * initValues populates the UI components.
+     */
     private void initValues() {
-        Log.i(this.getClass().getSimpleName(), "Initializing values.");
+        Log.i(this.getClass().getSimpleName(), "initValues: Initializing values.");
+
         // Initializing all objects from fragment_transfer
         tvFromAccountNo.setText(Integer.toString(transferFundsVO.getFromAccount().getAccNo()));
         tvFromCustomerNo.setText(Integer.toString(transferFundsVO.getFromAccount().getCustNo()));
@@ -79,45 +96,77 @@ public class TransferActivity extends AppCompatActivity {
         tvToCustomerNo.setText(Integer.toString(transferFundsVO.getToAccount().getCustNo()));
         tvTransferDetails.setText(transferFundsVO.getTransferDetails());
         tvTransferAmount.setText(Float.toString(transferFundsVO.getTransferAmount()));
-
     }
 
+    /**
+     * onClickTransfer handles tasks to be performed when user clicks Transfer button
+     */
     private void onClickTransfer() {
-        Log.d(this.getClass().getSimpleName(), "onClickTransfer: Transferring");
+        Log.d(this.getClass().getSimpleName(), "onClickTransfer: Transferring funds.");
+
+        // Creating an instance of TransferTask
         TransferTask transferTask = new TransferTask(this, commonVO.getServerAddress(),
                 getString(R.string.transfer_funds_path), transferFundsVO);
         transferTask.execute();
     }
 
+    /**
+     * TransferTask extends PostAsyncCommonTask to communicate with the server to perform post
+     * and transfer funds for the user.
+     */
     private class TransferTask extends PostAsyncCommonTask<TransferFundsVO> {
 
-        public TransferTask(Context contextObj, String serverAddress, String path, TransferFundsVO objToBeSent) {
+        /**
+         * TransferTask is the parametrized constructor of TransferTask
+         *
+         * @param contextObj    Contains the context of the parent activity.
+         * @param serverAddress Contains the server url/address .
+         * @param path          Contains the sub-path to the service that needs to be used.
+         * @param objToBeSent   Object of the VO class being sent to the server
+         */
+        public TransferTask(Context contextObj, String serverAddress,
+                            String path, TransferFundsVO objToBeSent) {
             super(contextObj, serverAddress, path, objToBeSent, TransferFundsVO.class);
         }
 
+        /**
+         * postSuccess is called when the server responds with a non-error code response.
+         * This function performs all the tasks to be done in postExecute when server response
+         * is not an error.
+         *
+         * @param amountTransferred Contains the string sent from the server as part of the
+         *                          response.
+         */
         @Override
         protected void postSuccess(String amountTransferred) {
             super.postSuccess(amountTransferred);
 
-            //Handles the case when transfer could not take place
+            // Handles the case when transfer could not take place
             if (amountTransferred.equals("false")) {
-                Toast.makeText(TransferActivity.this.getApplicationContext(), getString(R.string.transfer_unsuccessful), Toast.LENGTH_LONG).show();
+                Toast.makeText(TransferActivity.this.getApplicationContext(),
+                        getString(R.string.transfer_unsuccessful), Toast.LENGTH_LONG).show();
             } else {
                 // Case when transfer is successful
                 if (amountTransferred.equals("true")) {
-                    Toast.makeText(TransferActivity.this.getApplicationContext(), getString(R.string.transfer_successful), Toast.LENGTH_LONG).show();
+                    Toast.makeText(TransferActivity.this.getApplicationContext(),
+                            getString(R.string.transfer_successful), Toast.LENGTH_LONG).show();
 
-                    Intent intentNew = new Intent(TransferActivity.this.getApplicationContext(), HomePage.class);
+                    // Creating an intent to be injecting in the notification
+                    Intent intentNew = new Intent(TransferActivity.this.getApplicationContext(),
+                            HomePage.class);
                     intentNew.putExtra(getString(R.string.common_VO), commonVO);
                     intentNew.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    PendingIntent pIntent = PendingIntent.getActivity(TransferActivity.this.getApplicationContext(), (int) System.currentTimeMillis(), intentNew, 0);
 
+                    PendingIntent pIntent = PendingIntent.getActivity(
+                            TransferActivity.this.getApplicationContext(),
+                            (int) System.currentTimeMillis(), intentNew, 0);
+
+                    // Notification message
                     String transferMessage = "Amount Transferred from Acc No "
                             + tvFromAccountNo.getText() + " to Acc No "
                             + tvToAccountNo.getText();
 
-                    // build notification 
-                    // the addAction re-use the same intent to keep the example short
+                    // Creating a notification
                     Notification n = new Notification.Builder(getApplicationContext())
                             .setContentTitle(getString(R.string.transfer_successful))
                             .setContentText(transferMessage)
@@ -130,11 +179,16 @@ public class TransferActivity extends AppCompatActivity {
                             (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
                     notificationManager.notify(0, n);
+
+                    // Transferring control to HomePageActivity
                     Intent intent = new Intent(getApplicationContext(), HomePage.class);
                     intent.putExtra(getString(R.string.common_VO), commonVO);
                     startActivity(intent);
+
+                    // Terminating TransferActivity
+                    TransferActivity.this.finish();
                 } else {
-                    Log.e(this.getClass().getSimpleName(), "Invalid response on transfer funds");
+                    Log.e(this.getClass().getSimpleName(), "postSuccess: Invalid response on transfer funds");
                 }
             }
         }
