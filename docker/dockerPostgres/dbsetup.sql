10c7847d3b7c4894c58652e24141c35ac31196c2
voraj <voraj@iu.edu>
2016-04-15 18:46:37 -0400

    Updated Docker postgres
    #106

---
+++ docker/dockerPostgres/dbsetup.sql
@@ -0,0 +1,124 @@
+
+GRANT ALL PRIVILEGES ON DATABASE insecurepaydatabase TO insecurepay;
+
+-- Creation of tables
+CREATE TABLE customer
+(
+  cust_no integer NOT NULL,
+  cust_name character varying(30),
+  street character varying(30),
+  city character varying(30),
+  state character varying(30),
+  zipcode integer,
+  phone_no bigint,
+  email character varying(40),
+  birth_date date,
+  ssn character varying(50),
+  CONSTRAINT customer_pkey PRIMARY KEY (cust_no)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE customer OWNER TO insecurepay;
+
+CREATE TABLE public.cust_credentials
+(
+  cust_username character varying(30) NOT NULL,
+  password character varying(30),
+  cust_no integer,
+  CONSTRAINT cust_credentials_pkey PRIMARY KEY (cust_username),
+  CONSTRAINT "Cust_no_Fkey" FOREIGN KEY (cust_no)
+      REFERENCES public.customer (cust_no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.cust_credentials OWNER TO insecurepay;
+
+CREATE INDEX "fki_Cust_no_Fkey" ON public.cust_credentials USING btree(cust_no);
+
+CREATE TABLE public.account
+(
+  account_no integer NOT NULL,
+  cust_no integer,
+  account_balance numeric(10,2),
+  CONSTRAINT account_pkey PRIMARY KEY (account_no),
+  CONSTRAINT account_cust_no_fkey FOREIGN KEY (cust_no)
+      REFERENCES public.customer (cust_no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+
+ALTER TABLE public.account OWNER TO insecurepay;
+
+CREATE SEQUENCE public.seq_transferid
+  INCREMENT 1
+  MINVALUE 10
+  MAXVALUE 9223372036854775807
+  START 76
+  CACHE 1;
+ALTER TABLE public.seq_transferid OWNER TO insecurepay;
+
+CREATE TABLE public.transfer_funds
+(
+  transferid bigint NOT NULL DEFAULT nextval('seq_transferid'::regclass),
+  from_account_no integer,
+  from_cust_no integer,
+  to_account_no integer,
+  to_cust_no integer,
+  transfer_amount numeric(10,2),
+  from_beforeamount numeric(10,2),
+  from_afteramount numeric(10,2),
+  to_beforeamount numeric(10,2),
+  to_afteramount numeric(10,2),
+  transfer_details character varying(30),
+  transfer_date timestamp with time zone DEFAULT now(),
+  CONSTRAINT "TransferID_PK" PRIMARY KEY (transferid),
+  CONSTRAINT "from_cust_no_FK" FOREIGN KEY (from_cust_no)
+      REFERENCES public.customer (cust_no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT to_cust_no FOREIGN KEY (to_cust_no)
+      REFERENCES public.customer (cust_no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transfer_funds_from_account_no_fkey FOREIGN KEY (from_account_no)
+      REFERENCES public.account (account_no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT transfer_funds_to_account_no_fkey FOREIGN KEY (to_account_no)
+      REFERENCES public.account (account_no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.transfer_funds OWNER TO insecurepay;
+
+--Inserting queries into respective tables
+
+INSERT INTO customer VALUES (1, 'Cigital1', 'abc', 'Bloomington', 'Indiana', 47403, 9876545678, 'cigital1@cigital.com', '1980-01-10', 'WFtaWFta');
+INSERT INTO customer VALUES (2, 'Cigital2', 'XYZ', 'Bloomington', 'Indiana', 47403, 9876545645, 'cigital2@cigital.com', '1980-01-10', 'WVpbWVpb');
+INSERT INTO customer VALUES (3, 'Cigital3', 'E 5th street', 'Bloomington', 'Indiana', 47408, 54321679, 'cigital3@cigital.com', '2001-01-03', 'U1BRU1BR');
+INSERT INTO customer VALUES (4, 'Test', 'No where', 'Imaginary City', 'Funky State', 9133, 6666666666, 'test@iu.edu', '2016-03-08', 'UFFRUFFU');
+INSERT INTO customer VALUES (5, 'Cigital Class', 'Manhattan', 'New York City', 'New York', 1234, 123456, 'cigital@gmail.com', '1994-10-15', 'UFBQVVdV');
+
+INSERT INTO cust_credentials VALUES ('cigital1', 'cigital1', 1);
+INSERT INTO cust_credentials VALUES ('cigital2', 'cigital2', 2);
+INSERT INTO cust_credentials VALUES ('cigital3', 'cigital3', 3);
+INSERT INTO cust_credentials VALUES ('testUser', '12345', 4);
+INSERT INTO cust_credentials VALUES ('cigital', 'cigital', 5);
+
+INSERT INTO account VALUES (2001, 1, 2000.50);
+INSERT INTO account VALUES (2002, 2, 1700.96);
+INSERT INTO account VALUES (2003, 3, 3400.03);
+INSERT INTO account VALUES (2004, 4, 450.89);
+INSERT INTO account VALUES (2005, 5, 1234.59);
+
+INSERT INTO transfer_funds VALUES (5, 2001, 1, 2002, 2, -4.23, 1076.14, 1080.37, 1705.19, 1700.96, 'Thank you so much!', '2016-03-29 10:42:06.325524-04');
+INSERT INTO transfer_funds VALUES (6, 2003, 3, 2005, 5, 5.00, 1850.50, 1845.50, 2535.32, 2540.32, 'test', '2016-03-14 22:34:46.902308-04');
+INSERT INTO transfer_funds VALUES (7, 2004, 4, 2005, 5, 25.00, 3000.00, 2975.00, 2540.32, 2565.32, 'test', '2016-03-14 23:36:06.933278-04');
+
+
+
+  
---
+++ docker/dockerPostgres/docker-entrypoint.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+chown -R postgres "$PGDATA"
+
+if [ -z "$(ls -A "$PGDATA")" ]; then
+    gosu postgres initdb
+    sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
+
+    if [ "$POSTGRES_PASSWORD" ]; then
+      pass="PASSWORD $POSTGRES_PASSWORD"
+      authMethod=md5
+    else
+      echo "==============================="
+      echo "!!! Use \$POSTGRES_PASSWORD env var to secure your database !!!"
+      echo "==============================="
+      pass=
+      authMethod=trust
+    fi
+    echo
+	
+    if [ "$POSTGRES_USERNAME" != 'postgres' ]; then
+      op=CREATE
+    else
+      op=ALTER
+    fi
+
+    userSql="$op USER $POSTGRES_USERNAME WITH SUPERUSER;"
+    echo $userSql | gosu postgres postgres --single -jE
+    echo
+
+
+    if [ "$POSTGRES_DATABASE" != 'postgres' ]; then
+      createSql="CREATE DATABASE $POSTGRES_DATABASE OWNER $POSTGRES_USERNAME;"
+      echo $createSql | gosu postgres postgres --single -jE
+      echo
+    fi
+
+    # internal start of server in order to allow set-up using psql-client
+    # does not listen on TCP/IP and waits until start finishes
+    gosu postgres pg_ctl -D "$PGDATA" \
+        -o "-c listen_addresses=''" \
+        -w start
+
+    echo
+    
+	echo "============"
+	pwd
+	echo "value of input variable $SQL_FILE"
+	
+	if [[ -a $SQL_FILE ]]; then
+		gosu postgres psql --username "$POSTGRES_USERNAME" --dbname "$POSTGRES_DATABASE" -f $SQL_FILE 
+    fi
+
+	echo "=============="
+	gosu postgres psql --list
+	
+    gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
+
+    { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA"/pg_hba.conf
+fi
+
+gosu postgres pg_ctl -D "$PGDATA" start
