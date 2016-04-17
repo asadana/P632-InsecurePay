d0aec33dd5b90f01d6c0a344fa39af372becb7ef
Ankit Sadana <asadana@indiana.edu>
2016-04-17 08:38:48 -0400

    #103 Added secondary check for InterestCalcFragmentTest

--- InsecurePayAndroid/app/src/androidTest/java/com/cigital/insecurepay/fragments/InterestCalcFragmentTest.java
+++ InsecurePayAndroid/app/src/androidTest/java/com/cigital/insecurepay/fragments/InterestCalcFragmentTest.java
@@ -1,8 +1,6 @@
 package com.cigital.insecurepay.fragments;
 
-import android.app.Activity;
 import android.support.test.espresso.contrib.DrawerActions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -21,27 +19,20 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.*;
+import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 public class InterestCalcFragmentTest {
 
     @Rule
     public final ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
-    private Activity activityObj;
 
     @Test
     public void calcInterest() {
-
-        activityObj = loginActivityActivityTestRule.getActivity();
 
         Calendar calendarObj = Calendar.getInstance();
         Constants.principal = calendarObj.get(Calendar.MILLISECOND)*calendarObj.get(Calendar.MILLISECOND);
@@ -65,31 +56,31 @@ public class InterestCalcFragmentTest {
                 .perform(click());
 
 
-        //Enter some principal amount
+        // Enter some principal amount
         onView(withId(R.id.etIntCalc_FillPrincipalAmount)).
                 perform(replaceText(String.valueOf(Constants.principal)), closeSoftKeyboard());
 
 
-        //Enter some period for days/months/year
+        // Enter some period for days/months/year
         onView(withId(R.id.etIntCalc_Date)).
                 perform(typeText(String.valueOf(Constants.period)), closeSoftKeyboard());
-        //Check that ROI and Interest are empty
+
+        // Check that ROI and Interest are empty before Calculate button is clicked
         onView(withId(R.id.tvIntCalc_FillRateOfInterest))
                 .check(matches((withText(""))));
-
         onView(withId(R.id.tvIntCalc_FillInterest))
                 .check(matches((withText(""))));
-        //Hit Calculate
+
+        // Press Calculate Button
         onView(withId(R.id.btnIntCalc_Calc))
                 .perform(click());
 
-        //After clicking 'Calculate' Check that ROI and Interest are  displayed
-        onView(withId(R.id.tvIntCalc_FillRateOfInterest)).check(matches(isDisplayed()));
-        onView(withId(R.id.tvIntCalc_FillInterest)).check(matches(isDisplayed()));
-
+        // After clicking 'Calculate' Check that ROI and Interest are  displayed and is not empty
+        onView(withId(R.id.tvIntCalc_FillRateOfInterest))
+                .check(matches(isDisplayed())).check(matches(not(withText(""))));
+        onView(withId(R.id.tvIntCalc_FillInterest))
+                .check(matches(isDisplayed())).check(matches(not(withText(""))));
 
         Constants.logout();
     }
-
-
 }
