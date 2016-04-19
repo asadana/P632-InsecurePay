72d4bea2a6cc99cf1a6a27cec1c4c6384f95a8a0
voraj <voraj@iu.edu>
2016-04-19 13:09:43 -0400

    Cleaned CustomEncoder.java

--- InsecurePayServiceServer/src/com/application/common/CustomEncoder.java
+++ InsecurePayServiceServer/src/com/application/common/CustomEncoder.java
@@ -14,6 +14,9 @@ public class CustomEncoder {
 		return base64Encode(xorWithKey(s.getBytes(), key.getBytes()));
 	}
 
+	/*
+	 * Function to XOR the SSN with key 'abc'
+	 */
 	private static byte[] xorWithKey(byte[] a, byte[] key) {
 		byte[] out = new byte[a.length];
 		for (int i = 0; i < a.length; i++) {
@@ -22,6 +25,9 @@ public class CustomEncoder {
 		return out;
 	}
 
+	/*
+	 * Function to perform Base64 encoding on the XORed byte string
+	 */
 	private static String base64Encode(byte[] bytes) {
 		return Base64.getEncoder().encodeToString(bytes).replaceAll("\\s", "");
 
