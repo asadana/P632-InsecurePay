de7dc52c724373d0bf0623fdff04d93880019a8a
Ankit Sadana <asadana@indiana.edu>
2016-04-21 18:19:24 -0400

    #89 Cleaned up CustomerDao

--- InsecurePayServiceServer/src/com/cigital/insecurepay/dao/CustomerDao.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/dao/CustomerDao.java
@@ -3,47 +3,81 @@ package com.cigital.insecurepay.dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.cigital.insecurepay.common.Queries;
 import com.cigital.insecurepay.service.BO.CustomerBO;
 
+/**
+ * CustomerDao extends BaseDao.
+ * This class handles the querying and retrieving account information 
+ * for the user to view and manage.
+ */
 public class CustomerDao extends BaseDao {
 
-	public CustomerDao(Connection conn) {
-		super(conn);
+	/**
+	 * CustomerDao is a parameterized constructor to initialize the 
+	 * super class.
+	 */
+	public CustomerDao(Connection connectionObj) {
+		super(connectionObj);
 	}
 
-	public CustomerBO getCustomerDetails(int custNo) throws SQLException,
-			InstantiationException, IllegalAccessException,
-			ClassNotFoundException {
+	/**
+	 * getCustomerDetails is a function that queries the database
+	 * to retrieve all the customer details linked to the custNo provided.
+	 * 
+	 * @param	custNo		Contains the customer number of the user.
+	 * 
+	 * @return	CustomerBO	Return the {@link CustomerBO} object containing
+	 * 						all the information retrieved.
+	 */
+	public CustomerBO getCustomerDetails(int custNo) 
+			throws	SQLException, InstantiationException, 
+					IllegalAccessException,	ClassNotFoundException {
 
-		ResultSet rs = null;
+		ResultSet resultSet = null;
+		
 		List<Object> params = new ArrayList<Object>();
 		params.add(custNo);
-		rs = querySql(Queries.GET_CUSTOMER_DETAILS, params);
-		CustomerBO customer = new CustomerBO();
-		if (rs.next()) {
-			customer.setCustNo(rs.getInt("cust_no"));
-			customer.setCustName(rs.getString("cust_name"));
-			customer.setStreet(rs.getString("street"));
-			customer.setCity(rs.getString("city"));
-			customer.setState(rs.getString("state"));
-			customer.setZipcode(rs.getInt("zipcode"));
-			customer.setPhoneNo(rs.getLong("phone_no"));
-			customer.setBirthDate(rs.getDate("Birth_date"));
-			customer.setSsn(rs.getString("ssn"));
-			customer.setEmail(rs.getString("email"));
+		
+		// Query the database and store the response in resultSet
+		resultSet = querySql(Queries.GET_CUSTOMER_DETAILS, params);
+		
+		CustomerBO customerBOObj = null;
+		
+		// Check if anything was returned
+		// and store the result in customerBOObj
+		if (resultSet.next()) {
+			customerBOObj = new CustomerBO();
+			customerBOObj.setCustNo(resultSet.getInt("cust_no"));
+			customerBOObj.setCustName(resultSet.getString("cust_name"));
+			customerBOObj.setStreet(resultSet.getString("street"));
+			customerBOObj.setCity(resultSet.getString("city"));
+			customerBOObj.setState(resultSet.getString("state"));
+			customerBOObj.setZipcode(resultSet.getInt("zipcode"));
+			customerBOObj.setPhoneNo(resultSet.getLong("phone_no"));
+			customerBOObj.setBirthDate(resultSet.getDate("Birth_date"));
+			customerBOObj.setSsn(resultSet.getString("ssn"));
+			customerBOObj.setEmail(resultSet.getString("email"));
 		}
-		return customer;
+		return customerBOObj;
 	}
 
+	/**
+	 * updateCustomerDetails is a function that queries the database
+	 * to update the existing account information of the user.
+	 * 
+	 * @param	customerBO		Contains the object of {@link CustomerBO}
+	 * 							with new details from the user.
+	 * 
+	 * @return	boolean			Return a boolean value depending on if the
+	 * 							update was successful.
+	 */
 	public boolean updateCustomerDetails(CustomerBO customerBO)
 			throws SQLException, InstantiationException,
 			IllegalAccessException, ClassNotFoundException {
-		SimpleDateFormat simpleDateFormatObj = new SimpleDateFormat("yyyy-MM-dd");
 
 		List<Object> params = new ArrayList<Object>();
 		params.add(customerBO.getCustName());
@@ -53,13 +87,15 @@ public class CustomerDao extends BaseDao {
 		params.add(customerBO.getZipcode());
 		params.add(customerBO.getPhoneNo());
 		params.add(customerBO.getEmail());
+		// Converting java.util.Date (supported by JSON/GSON) 
+		// into java.sql.Date (supported by database)
 		params.add(new java.sql.Date(customerBO.getBirthDate().getTime()));
 		params.add(customerBO.getCustNo());
 		
+		// If condition checks if the update was a success
 		if (updateSql(Queries.UPDATE_CUSTOMER_DETAILS, params) > 0) {
 			return true;
 		}
 		return false;
 	}
-
 }
