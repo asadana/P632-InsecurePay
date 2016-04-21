2fe656a073e5321efc8fd817a86641a68052dd48
Ankit Sadana <asadana@indiana.edu>
2016-04-21 16:48:53 -0400

    #89 Cleaned up ActivityHistoryDao

--- InsecurePayServiceServer/src/com/cigital/insecurepay/dao/ActivityHistoryDao.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/dao/ActivityHistoryDao.java
@@ -10,34 +10,53 @@ import java.util.List;
 import com.cigital.insecurepay.common.Queries;
 import com.cigital.insecurepay.service.BO.TransactionBO;
 
+/**
+ * ActivityHistoryDao extends BaseDao.
+ * This class handles the querying and retrieving Account History for the user.
+ */
 public class ActivityHistoryDao extends BaseDao {
 
-	public ActivityHistoryDao(Connection conn) {
-		super(conn);
+	/**
+	 * ActivityHistoryDao is a parameterized constructor to initialize the 
+	 * super class.
+	 */
+	public ActivityHistoryDao(Connection connectionObj) {
+		super(connectionObj);
 	}
 
+	/**
+	 * getActivityHistory is a function that queries the database for the given
+	 * account number.
+	 * 
+	 * @param	accountNo	Contains the account number given by the user.
+	 * 
+	 * @return	List<TransactionBO>	Returns a List of {@link TransactionBO} object.
+	 */
 	public List<TransactionBO> getActivityHistory(int accountNo)
 			throws InstantiationException, IllegalAccessException,
 			ClassNotFoundException, SQLException {
 
 		List<TransactionBO> resultList = new ArrayList<TransactionBO>();
-		ResultSet rs = null;
-		Date transferdt;
+		ResultSet resultSet = null;
+		Date transferDate;
+		
 		List<Object> params = new ArrayList<Object>();
 		params.add(accountNo);
 		params.add(accountNo);
-		rs = querySql(Queries.GET_ACTIVITY_HISTORY, params);
 		
-		while (rs.next()) {
+		resultSet = querySql(Queries.GET_ACTIVITY_HISTORY, params);
+		
+		// while loop traverses resultSet and grabs individual details
+		// for each object.
+		while (resultSet.next()) {
 			TransactionBO transaction = new TransactionBO();
-			transaction.setDescription(rs.getString("transfer_details"));
-			transferdt = rs.getDate("transfer_date");
-			transaction.setDate(transferdt.toString());
-			transaction.setFinalAmount(rs.getFloat("final_amount"));
-			transaction.setTransactionAmount(rs.getFloat("transfer_amount"));
-			transaction.setType(rs.getInt("type"));
+			transaction.setDescription(resultSet.getString("transfer_details"));
+			transferDate = resultSet.getDate("transfer_date");
+			transaction.setDate(transferDate.toString());
+			transaction.setFinalAmount(resultSet.getFloat("final_amount"));
+			transaction.setTransactionAmount(resultSet.getFloat("transfer_amount"));
+			transaction.setType(resultSet.getInt("type"));
 			resultList.add(transaction);
-
 		}
 		close();
 
