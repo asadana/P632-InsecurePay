54460402b12b8437eac2f7edfb0749a2412edaf3
Ankit Sadana <asadana@indiana.edu>
2016-04-21 18:07:12 -0400

    #89 Cleaned up ChangePasswordDao

--- InsecurePayServiceServer/src/com/cigital/insecurepay/dao/ChangePasswordDao.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/dao/ChangePasswordDao.java
@@ -9,23 +9,42 @@ import java.util.List;
 import com.cigital.insecurepay.common.Queries;
 import com.cigital.insecurepay.service.BO.ChangePasswordBO;
 
-
+/**
+ * ChangePasswordDao extends BaseDao.
+ * This class queries the database to update the user's password.
+ */
 public class ChangePasswordDao extends BaseDao{
 	
+	/**
+	 * ChangePasswordDao is a parameterized constructor to initialize the 
+	 * super class.
+	 */
 	public ChangePasswordDao(Connection conn) {
 		super(conn);
 	}
 	
-	public Boolean ChangePassword(ChangePasswordBO l)
+	/**
+	 * ChangePasswordDao is a function that queries the database and updates the 
+	 * password for the user.
+	 * 
+	 * @param	changePasswordBOObj		Contains an object of ChangePassword
+	 * 									class.
+	 * 
+	 * @return	Boolean		Return a boolean value depending if the query
+	 * 						was successful.
+	 */
+	public Boolean ChangePassword(ChangePasswordBO changePasswordBOObj)
 			throws InstantiationException, IllegalAccessException,
 			ClassNotFoundException, SQLException {
 		
 		Boolean password_changed = false;
 
 		List<Object> params = new ArrayList<Object>();
-		params.add(l.getPassword());
-		params.add(l.getUsername());
+		params.add(changePasswordBOObj.getPassword());
+		params.add(changePasswordBOObj.getUsername());
+		
 		int count = updateSql(Queries.UPDATE_PASSWORD, params);
+		
 		if (count>=1) 
 			password_changed = true;
 		else
@@ -33,5 +52,4 @@ public class ChangePasswordDao extends BaseDao{
 	
 		return password_changed;
 	}
-
 }
