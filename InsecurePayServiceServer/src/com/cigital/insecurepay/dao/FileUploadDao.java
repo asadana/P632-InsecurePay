f5788338ff8f7d1691d6c7772773fae914d41eb1
Ankit Sadana <asadana@indiana.edu>
2016-04-21 18:41:43 -0400

    #89 Cleaned up FileUploadDao

--- InsecurePayServiceServer/src/com/cigital/insecurepay/dao/FileUploadDao.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/dao/FileUploadDao.java
@@ -10,26 +10,54 @@ import java.sql.Connection;
 import com.cigital.insecurepay.common.Constants;
 import com.cigital.insecurepay.service.Logging;
 
+/**
+ * FileUploadDao extends BaseDao.
+ * This class takes the InputStream from the user
+ * and stores it into a file with a given fileName.
+ */
 public class FileUploadDao extends BaseDao {
 
+	// String stores the complete path of the file being stored.
 	String uploadedFileLocation;
 
+	/**
+	 * FileUploadDao is a parameterized constructor to initialize the 
+	 * super class.
+	 */
 	public FileUploadDao(Connection conn) {
 		super(conn);
 		uploadedFileLocation = null;
 	}
 
+	/**
+	 * saveUploadFile is a function that takes the InputStream and fileName 
+	 * and performs write access checks, then passes the InputStream and fileName to
+	 * writeToFile function.
+	 * 
+	 * @param	uploadedInputStream		Contains the InputStream coming from the user.
+	 * @param	fileName				Contains the file name received from the user.
+	 * 
+	 * @return	Boolean		Return a boolean object depending on if the file was 
+	 * 						successfully saved.
+	 */
 	public boolean saveUploadedFile(InputStream uploadedInputStream, String fileName) {
+		
+		// Folder location from the Constants class
 		File folderFileObj = new File(Constants.fileUploadDir);
 
 		Logging.logger.debug("saveUploadedFile: Folder path: " + Constants.fileUploadDir);
 		Logging.logger.debug("saveUploadedFile: Folder exists?: " + folderFileObj.exists());
 		Logging.logger.debug("saveUploadedFile: Folder is a directory?: " + folderFileObj.isDirectory());
 		
+		// If condition checks if the the folder path doesn't exists or is not a directory
 		if (!(folderFileObj.exists() && folderFileObj.isDirectory())) {
 			Logging.logger.debug("saveUploadedFile: folder not found");
+			
+			// If condition attempts to make a directory
 			if (folderFileObj.mkdir()) {
 				Logging.logger.debug("saveUploadedFile: Creating folder");
+				
+				// Check again if the folder path exists or is not a directory
 				if (!(folderFileObj.exists() && folderFileObj.isDirectory())) {
 					Logging.logger.debug("saveUploadedFile: Unable to create a folder");
 					return false;
@@ -40,43 +68,54 @@ public class FileUploadDao extends BaseDao {
 			}
 		}
 		
+		// Setting up the file path in the fileUploadDir folder
 		uploadedFileLocation = Constants.fileUploadDir + "/" + fileName;
 		
+		// boolean object to store if the file write was successful or not
 		boolean booleanObj = writeToFile(uploadedInputStream, uploadedFileLocation); 
-		if (booleanObj) {
-			Logging.logger.debug("storeUploadedFile: writeToFile returned true");
-			return booleanObj;
-		} else {
-			Logging.logger.debug("storeUploadedFile: writeToFile returned false");
-			return booleanObj;
-		}
+		
+		Logging.logger.debug("storeUploadedFile: writeToFile returned " + booleanObj);
+		return booleanObj;
 	}
 
-	// save uploaded file to new location
-	private boolean writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
+	/**
+	 * writeToFile is a function that takes the InputStream and file path
+	 * and saves it as file to with the given path.
+	 * 
+	 * @param	uploadedInputStream		Contains the InputStream with the file content.
+	 * @param	uploadedFileLocation	Contains the file path to be used to save the file.
+	 *
+	 * @return	Boolean		Return a boolean object depending on if the file was 
+	 * 						successfully saved.
+	 */
+	private boolean writeToFile(InputStream uploadedInputStream, 
+								String uploadedFileLocation) {
 
 		try {
-			
-			OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+			OutputStream outputStream = new FileOutputStream(new File(uploadedFileLocation));
 			Logging.logger.debug("writeToFile: File created.");
-			int read = 0;
-			byte[] bytes = new byte[1024];
+			
+			int readBytes = 0;
+			byte[] bytesArray = new byte[1024];
 
-			out = new FileOutputStream(new File(uploadedFileLocation));
+			outputStream = new FileOutputStream(new File(uploadedFileLocation));
+			
 			Logging.logger.debug("writeToFile: File writing started.");
-			while ((read = uploadedInputStream.read(bytes)) != -1) {
-				out.write(bytes, 0, read);
+			
+			// While loop reads the bytes from the uploadedInputStream
+			while ((readBytes = uploadedInputStream.read(bytesArray)) != -1) {
+				outputStream.write(bytesArray, 0, readBytes);
 			}
-			out.flush();
-			out.close();
+			
+			outputStream.flush();
+			outputStream.close();
 			Logging.logger.debug("writeToFile: File writing completed.");
+			
 			return true;
 		} catch (IOException e) {
 			Logging.logger.debug("writeToFile: Unable to write to a file");
 			Logging.logger.error(e);
 			return false;
 		}
-
 	}
-
 }
