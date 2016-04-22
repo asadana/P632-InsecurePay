4fdf9fd250cb5ef378e8d5d7f4f8464ac995a48f
voraj <voraj@iu.edu>
2016-04-22 18:43:38 -0400

    #89 cleaned up GetAsyncCommonTask

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/common/PostAsyncCommonTask.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/common/PostAsyncCommonTask.java
@@ -4,10 +4,21 @@ import android.content.Context;
 
 import java.net.HttpURLConnection;
 
+/**
+ * PostAsyncCommonTask is used to handle all the POST requests Async tasks
+ */
 public abstract class PostAsyncCommonTask<T> extends AsyncCommonTask {
     protected T objToBeSent;
     private Class<T> classObj;
 
+    /**
+     * PostAsyncCommonTask is the parameterized constructor
+     *
+     * @param contextObj    Contains the context of the parent activity.
+     * @param serverAddress Contains the server url/address .
+     * @param path          Contains the sub-path to the service that needs to be used.
+     * @param objToBeSent Data or Object of the VO class being sent to the server
+     */
     public PostAsyncCommonTask(Context contextObj, String serverAddress, String path,
                                T objToBeSent, Class<T> classObj) {
         super(contextObj, serverAddress, path);
