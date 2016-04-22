07ad474aa77cf2eba73e4f43773f4508e69a44fe
Ankit Sadana <asadana@indiana.edu>
2016-04-22 16:23:21 -0400

    #89 Cleaned up AccountNumValidationService

--- InsecurePayServiceServer/src/com/cigital/insecurepay/service/AccountNumValidationService.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/service/AccountNumValidationService.java
@@ -8,14 +8,25 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import com.cigital.insecurepay.common.Constants;
 import com.cigital.insecurepay.common.DaoFactory;
 import com.cigital.insecurepay.dao.AccountDao;
 
+/**
+ * AccountNumValidationService extends {@link BaseService}.
+ * This class is a service that validates the account number
+ * sent by the user by querying the database.
+ */
 @Path("/accountNumberValidation")
-public class AccountNoValidationService extends BaseService {
+public class AccountNumValidationService extends BaseService {
 
+	/**
+	 * accountNumberValid is a function that calls an instance of 
+	 * {@link AccountDao} to validate the account number.
+	 * 
+	 */
 	@GET
-	public Response AccountNoValid(@QueryParam("accountNo") int accountNo) {
+	public Response accountNumberValid(@QueryParam("accountNo") int accountNo) {
 
 		Boolean accountValid = false;
 		try {
@@ -25,17 +36,19 @@ public class AccountNoValidationService extends BaseService {
 				| ClassNotFoundException | NoSuchMethodException
 				| SecurityException | IllegalArgumentException
 				| InvocationTargetException | SQLException e) {
+			
 			logger.error(e);
+			return Response.status(Response.Status.BAD_REQUEST).build();
+			
 		} finally {
 
 			try {
 				close();
 			} catch (SQLException e) {
 				logger.error(e);
+				return Response.status(Response.Status.BAD_REQUEST).build();
 			}
 		}
 		return Response.status(Response.Status.OK).entity(accountValid).build();
-
 	}
-
 }
