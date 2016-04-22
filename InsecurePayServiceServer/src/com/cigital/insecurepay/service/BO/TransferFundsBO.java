37dca8bdb16fe1903c900a3ea29356bb1c4e0967
Ankit Sadana <asadana@indiana.edu>
2016-04-22 11:25:32 -0400

    #89 Cleaned up TransferFundsBO

--- InsecurePayServiceServer/src/com/cigital/insecurepay/service/BO/TransferFundsBO.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/service/BO/TransferFundsBO.java
@@ -2,56 +2,71 @@ package com.cigital.insecurepay.service.BO;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * TransferFundsBO is POJO to store details of the transfer being made for the
+ * user.
+ */
 @XmlRootElement
 public class TransferFundsBO {
-	
+
 	private AccountBO fromAccount;
-    private AccountBO toAccount;
-    private float transferAmount;
-    private String transferDetails;
+	private AccountBO toAccount;
+	private float transferAmount;
+	private String transferDetails;
 
-    public TransferFundsBO(AccountBO fromAccount, AccountBO toAccount, float transferAmount, String transferDetails) {
-        this.fromAccount = fromAccount;
-        this.toAccount = toAccount;
-        this.transferAmount = transferAmount;
-        this.transferDetails = transferDetails;
-    }
+	/**
+     * TransferFundsBO default constructor
+     */
+	public TransferFundsBO() {
+	}
 
-    public TransferFundsBO(){
+	 /**
+     * TransferFundsBO parameterized constructor
+     * 
+     * @param	fromAccount
+     * @param	toAccount
+     * @param	transferAmount
+     * @param	transferDetails
+     */
+	public TransferFundsBO(AccountBO fromAccount, AccountBO toAccount, 
+							float transferAmount, String transferDetails) {
+		super();
+		this.fromAccount = fromAccount;
+		this.toAccount = toAccount;
+		this.transferAmount = transferAmount;
+		this.transferDetails = transferDetails;
+	}
 
-    }
+	public AccountBO getFromAccount() {
+		return fromAccount;
+	}
 
-    public AccountBO getFromAccount() {
-        return fromAccount;
-    }
+	public void setFromAccountBO(AccountBO fromAccount) {
+		this.fromAccount = fromAccount;
+	}
 
-    public void setFromAccountBO(AccountBO fromAccount) {
-        this.fromAccount = fromAccount;
-    }
+	public AccountBO getToAccount() {
+		return toAccount;
+	}
 
-    public AccountBO getToAccount() {
-        return toAccount;
-    }
+	public void setToAccountBO(AccountBO toAccount) {
+		this.toAccount = toAccount;
+	}
 
-    public void setToAccountBO(AccountBO toAccount) {
-        this.toAccount = toAccount;
-    }
+	public float getTransferAmount() {
+		return transferAmount;
+	}
 
-    public float getTransferAmount() {
-        return transferAmount;
-    }
+	public void setTransferAmount(float transferAmount) {
+		this.transferAmount = transferAmount;
+	}
 
-    public void setTransferAmount(float transferAmount) {
-        this.transferAmount = transferAmount;
-    }
+	public String getTransferDetails() {
+		return transferDetails;
+	}
 
-    public String getTransferDetails() {
-        return transferDetails;
-    }
+	public void setTransferDetails(String transferDetails) {
+		this.transferDetails = transferDetails;
+	}
 
-    public void setTransferDetails(String transferDetails) {
-        this.transferDetails = transferDetails;
-    }
-
-	
 }
