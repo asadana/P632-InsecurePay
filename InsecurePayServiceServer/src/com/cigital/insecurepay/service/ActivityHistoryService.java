c41db00d4f2459d62362bce15d993995364c83ad
Ankit Sadana <asadana@indiana.edu>
2016-04-22 16:35:07 -0400

    #89 Cleaned up ActivityHistoryService

--- InsecurePayServiceServer/src/com/cigital/insecurepay/service/ActivityHistoryService.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/service/ActivityHistoryService.java
@@ -15,12 +15,21 @@ import com.cigital.insecurepay.common.DaoFactory;
 import com.cigital.insecurepay.dao.ActivityHistoryDao;
 import com.cigital.insecurepay.service.BO.TransactionBO;
 
+/**
+ * ActivityHistoryService extends {@link BaseService}.
+ * This class is a service that retrieves the activity on the
+ * account number sent by the user.
+ */
 @Path("/activityHistory")
 public class ActivityHistoryService extends BaseService{
 
+	/**
+	 * getActivityHistory is a function that calls an instance of 
+	 * {@link ActivityHistoryDao} to query the database.
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response ActivityList(@QueryParam("accountNo") int accountNo){
+	public Response getActivityHistory(@QueryParam("accountNo") int accountNo){
 		List<TransactionBO> resultlist = null;
 		try {
 			resultlist = DaoFactory.getInstance(ActivityHistoryDao.class,
@@ -30,13 +39,21 @@ public class ActivityHistoryService extends BaseService{
 				| SecurityException | IllegalArgumentException
 				| InvocationTargetException | SQLException e) {
 			logger.error(e);
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 		} finally {
 			try {
 				close();
 			} catch (SQLException e) {
 				logger.error(e);
+				return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 			}
 		}
-		return Response.status(Response.Status.ACCEPTED).entity(resultlist).build();
+		
+		if (resultlist != null) {
+			return Response.status(Response.Status.ACCEPTED).entity(resultlist).build();	
+		} else {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		
 	}
 }
