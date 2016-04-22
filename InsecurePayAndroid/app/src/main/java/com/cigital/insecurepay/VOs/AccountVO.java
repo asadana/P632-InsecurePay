5e0824b824f7beda2b01ee6101e016e3d79238d5
voraj <voraj@iu.edu>
2016-04-22 01:32:01 -0400

    #89 Cleaned Up AccountVO

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/VOs/AccountVO.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/VOs/AccountVO.java
@@ -2,33 +2,42 @@ package com.cigital.insecurepay.VOs;
 
 import java.io.Serializable;
 
+/**
+ * AccountVO is POJO to store basic account information of the user.
+ */
 public class AccountVO implements Serializable{
-    private int custNo;
-    private int accNo;
+    private int customerNumber;
+    private int accountNumber;
     private float accountBalance;
 
-
-    public AccountVO(int custNo, int accNo, float accountBalance) {
-        this.custNo = custNo;
-        this.accNo = accNo;
+    /**
+     * AccountVO parameterized constructor
+     *
+     * @param	customerNumber
+     * @param	accountNumber
+     * @param	accountBalance
+     */
+    public AccountVO(int customerNumber, int accountNumber, float accountBalance) {
+        this.customerNumber = customerNumber;
+        this.accountNumber = accountNumber;
         this.accountBalance = accountBalance;
 
     }
 
-    public int getCustNo() {
-        return custNo;
+    public int getCustomerNumber() {
+        return customerNumber;
     }
 
-    public void setCustNo(int custNo) {
-        this.custNo = custNo;
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
     }
 
-    public int getAccNo() {
-        return accNo;
+    public int getAccountNumber() {
+        return accountNumber;
     }
 
-    public void setAccNo(int accNo) {
-        this.accNo = accNo;
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
     }
 
     public float getAccountBalance() {
@@ -38,5 +47,4 @@ public class AccountVO implements Serializable{
     public void setAccountBalance(float accountBalance) {
         this.accountBalance = accountBalance;
     }
-
 }
