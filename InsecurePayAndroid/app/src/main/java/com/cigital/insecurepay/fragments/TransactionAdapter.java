52a21e248435264f8aeca09872707a67d17eabc1
voraj <voraj@iu.edu>
2016-04-22 15:58:02 -0400

    #89 cleaned up TransactionAdapter

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/TransactionAdapter.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/fragments/TransactionAdapter.java
@@ -13,12 +13,24 @@ import com.cigital.insecurepay.VOs.TransactionVO;
 
 import java.util.List;
 
+/**
+ * TransactionAdapter is called for making a View for each transfer in the transaction list.
+ */
 public class TransactionAdapter extends ArrayAdapter {
 
+    //View objects
     private List<TransactionVO> transactionVOList;
     private int resource;
     private LayoutInflater inflater;
+    private String DollarSymbol="$";
 
+    /**
+     * TransactionAdapter parameterized constructor
+     *
+     * @param context
+     * @param resource
+     * @param transactionVOList
+     */
     public TransactionAdapter(Context context, int resource, List<TransactionVO> transactionVOList) {
         super(context, resource, transactionVOList);
         this.resource = resource;
@@ -34,20 +46,23 @@ public class TransactionAdapter extends ArrayAdapter {
         }
 
         int type;
+        //Text placeholders
         TextView tvActivityHistory_Description;
         TextView tvActivityHistory_Date;
         TextView tvActivityHistory_FinalAmount;
         TextView tvActivityHistory_TransactionAmount;
 
+        //Adapter uses convertView to recycle old view objects no longer being used
         tvActivityHistory_Description = (TextView) convertView.findViewById(R.id.tvActivityHistory_Description);
         tvActivityHistory_Date = (TextView) convertView.findViewById(R.id.tvActivityHistory_Date);
         tvActivityHistory_FinalAmount = (TextView) convertView.findViewById(R.id.tvActivityHistory_FinalAmount);
         tvActivityHistory_TransactionAmount = (TextView) convertView.findViewById(R.id.tvActivityHistory_TransactionAmount);
 
+        //Transaction details are placed in appropriate textView holders
         tvActivityHistory_Description.setText(transactionVOList.get(position).getDescription());
         tvActivityHistory_Date.setText(transactionVOList.get(position).getDate());
-        tvActivityHistory_FinalAmount.setText("$" + transactionVOList.get(position).getFinalAmount());
-        tvActivityHistory_TransactionAmount.setText("$" + transactionVOList.get(position).getTransactionAmount());
+        tvActivityHistory_FinalAmount.setText(DollarSymbol + transactionVOList.get(position).getFinalAmount());
+        tvActivityHistory_TransactionAmount.setText( DollarSymbol + transactionVOList.get(position).getTransactionAmount());
         type = transactionVOList.get(position).getType();
         if (type == 1) //type 1 is debit and type 2 is credit
             tvActivityHistory_TransactionAmount.setTextColor(Color.RED);
