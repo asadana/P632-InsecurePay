78266268ee09ea91621e0a68bad184397a0e575f
Ankit Sadana <asadana@indiana.edu>
2016-04-22 16:45:04 -0400

    #89 Cleaned up ChangePasswordService

--- InsecurePayServiceServer/src/com/cigital/insecurepay/service/ChangePasswordService.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/service/ChangePasswordService.java
@@ -14,33 +14,46 @@ import com.cigital.insecurepay.common.DaoFactory;
 import com.cigital.insecurepay.dao.ChangePasswordDao;
 import com.cigital.insecurepay.service.BO.ChangePasswordBO;
 
+/**
+ * ChangePasswordService extends {@link BaseService}. This class is a service
+ * that allows the user to update his existing password.
+ */
 @Path("/changePassword")
 public class ChangePasswordService extends BaseService {
 
+	/**
+	 * changePassword is a function that calls an instance of
+	 * {@link ChangePasswordDao} to update the database with the new password.
+	 * 
+	 * @param	changePasswordBO	{@link ChangePasswordBO}
+	 */
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response changePassword(ChangePasswordBO changePasswordBO) {
+
 		Boolean passwordChanged = false;
+
 		try {
 			passwordChanged = DaoFactory.getInstance(ChangePasswordDao.class,
-					this.getConnection()).changePassword(changePasswordBO);
+														this.getConnection())
+										.changePassword(changePasswordBO);
 
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException | NoSuchMethodException
-				| SecurityException | IllegalArgumentException
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException
+				| NoSuchMethodException | SecurityException | IllegalArgumentException 
 				| InvocationTargetException | SQLException e) {
 			logger.error(e);
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 		} finally {
 
 			try {
 				close();
 			} catch (SQLException e) {
 				logger.error(e);
+				return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 			}
 		}
-		return Response.status(Response.Status.OK).entity(passwordChanged)
-				.build();
 
+		return Response.status(Response.Status.OK).entity(passwordChanged).build();
 	}
 }
