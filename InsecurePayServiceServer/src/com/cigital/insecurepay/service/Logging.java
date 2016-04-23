37ccd9da29511f8e9cf5e3400c643dba279977f3
Ankit Sadana <asadana@indiana.edu>
2016-04-23 07:07:47 -0400

    #89 Cleaned up Logging

--- InsecurePayServiceServer/src/com/cigital/insecurepay/service/Logging.java
+++ InsecurePayServiceServer/src/com/cigital/insecurepay/service/Logging.java
@@ -10,19 +10,30 @@ import javax.ws.rs.ApplicationPath;
 
 import java.util.logging.Logger;
 
+/**
+ * Logging extends {@link ResourceConfig}. This class is used to provide a
+ * logger for all the other classes.
+ */
 @ApplicationPath("/rest")
 public class Logging extends ResourceConfig {
-	public static final org.apache.logging.log4j.Logger logger = LogManager
-			.getLogger(BaseService.class.getName());
+	public static final org.apache.logging.log4j.Logger logger = 
+			LogManager.getLogger(BaseService.class.getName());
 
+	/**
+	 * Logging is the default unparameterized constructor that is used to
+	 * initializ the logging functionality.
+	 */
 	public Logging() {
 		SLF4JBridgeHandler.removeHandlersForRootLogger();
 		SLF4JBridgeHandler.install();
 
-		packages("com.cigital.insecurepay.service", "com.fasterxml.jackson.jaxrs.json",
+		packages("com.cigital.insecurepay.service", 
+				"com.fasterxml.jackson.jaxrs.json",
 				"com.cigital.insecurepay.service.BO");
+		
 		java.util.logging.Logger javaLogger = Logger
 				.getLogger(BaseService.class.getName());
+		
 		register(MultiPartFeature.class);
 		registerInstances(new LoggingFilter(javaLogger, true));
 	}
