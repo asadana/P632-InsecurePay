26780fa7a02bdbf587ba0cb8e9ce471c34ed9499
Ankit Sadana <asadana@indiana.edu>
2016-04-23 16:36:49 -0400

    #89 Cleaned up LoginDBHelper

--- InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/DBHelper/LoginDBHelper.java
+++ InsecurePayAndroid/app/src/main/java/com/cigital/insecurepay/DBHelper/LoginDBHelper.java
@@ -14,79 +14,100 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 /**
- * LoginDBHelper is a class that is used to keep track of login attempts and account lockout status in 'LoginTrials' table.
+ * LoginDBHelper is a class that extends {@link DBHelper} that is used to
+ * keep track of login attempts and account lockout status in 'LoginTrials' table.
  */
 public class LoginDBHelper extends DBHelper {
 
     //Initialize column variable names
     public static final String CUST_USERNAME = "cust_username";
-    public static final String TRIALS = "trials";
+    public static final String TABLE_NAME = "trials";
     public static final String CURR_TIME = "curr_time";
     public static final String isLocked = "isLocked";
-    DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
+    private DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
     private Context contextObj;
 
+    /**
+     * LoginDBHelper is the parametrized constructor of this class.
+     *
+     * @param context Contains the context of the parent.
+     */
     public LoginDBHelper(Context context) {
         super(context);
         this.contextObj = context;
     }
 
+    /**
+     * onUpgrade is an overridden function that is called when the database needs to be updated
+     * from a previous version.
+     *
+     * @param sqLiteDatabase Contains the database object.
+     * @param oldVersion     Contains the oldVersion number.
+     * @param newVersion     Contains the newVersion number.
+     */
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
     }
 
     /**
-     * addTrial is called to make entry in 'LoginTrials' table when first unsuccessful login attempt is made.
+     * addTrial is a function that is called to make entry in table when first
+     * unsuccessful login attempt is made.
      *
      * @param username Username used to login into the database
-     *
      */
     public void addTrial(String username) {
-        Log.d("LoginDBHelper", "addTrial");
-        SQLiteDatabase db = this.getWritableDatabase();
+        Log.d(this.getClass().getSimpleName(), "addTrial: Adding new user " + username);
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         //prepares row entry for insertion
-        values.put(TRIALS, 1);
+        values.put(TABLE_NAME, 1);
         values.put(CUST_USERNAME, username);
         values.put(isLocked, 0);
         values.put(CURR_TIME, format.print(DateTime.now()));
         //inserts the row in database
-        db.insert(contextObj.getString(R.string.tableLoginTrials), null, values);
+        sqLiteDatabase.insert(contextObj.getString(R.string.tableLoginTrials), null, values);
     }
 
     /**
-     * updateTrial is called to update the number of unsuccessful trails made for logging in
+     * updateTrial is a function that is called to update the number of
+     * unsuccessful trails made for logging in.
      *
-     * @param username Username used to login into the database
-     *        trial    Trail count
-     *        islocked Specifies if the account is locked or not
+     * @param username      Username used to login into the database.
+     * @param trialCount    Contains the number of trials.
+     * @param isLocked      Specifies if the account is locked or not.
      */
-    public void updateTrial(String username, int trial, boolean islocked) {
-        Log.d("LoginDBHelper", "updateTrial");
-        SQLiteDatabase db = this.getWritableDatabase();
+    public void updateTrial(String username, int trialCount, boolean isLocked) {
+        Log.d(this.getClass().getSimpleName(), "updateTrial: Updating trial count.");
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         //prepares row entry for insertion
-        values.put(TRIALS, trial);
-        values.put(isLocked, islocked);
+        values.put(TABLE_NAME, trialCount);
+        values.put(LoginDBHelper.isLocked, isLocked);
         values.put(CURR_TIME, format.print(DateTime.now()));
         //updates the row of the database
-        db.update(contextObj.getString(R.string.tableLoginTrials), values,
-                    CUST_USERNAME + "='" + username + "'", null);
+        sqLiteDatabase.update(contextObj.getString(R.string.tableLoginTrials), values,
+                CUST_USERNAME + "='" + username + "'", null);
     }
 
     /**
-     * getTrial is called to check number of unsuccessful login trials made to determine lockout
+     * getTrial is a function that is called to check number of unsuccessful
+     * login trials made to determine lockout.
      *
-     * @param username Username used to login into the database
+     * @param username Username used to login into the database.
      */
     public int getTrial(String username) {
-        Log.d("LoginDBHelper", "getTrial");
+        Log.d(this.getClass().getSimpleName(),
+                "getTrial: Retrieving the trials of user: " + username);
+
         int userTrial = -1;
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("select " + TRIALS + " from " +
-                                    contextObj.getString(R.string.tableLoginTrials) +
-                                    " where " + CUST_USERNAME + "='" + username + "'", null);
+
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("select " + TABLE_NAME + " from " +
+                contextObj.getString(R.string.tableLoginTrials) + " where " +
+                CUST_USERNAME + "='" + username + "'", null);
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
             userTrial = cursor.getInt(0);
@@ -97,37 +118,44 @@ public class LoginDBHelper extends DBHelper {
     }
 
     /**
-     * getTimestamp is used to get the timestamp when account got locked. If the time is greater than a particular
-     * duration then account is unlocked
+     * getTimestamp is a function that is used to get the timestamp when account got locked.
+     * If the time is greater than a particular duration then account is unlocked.
      *
      * @param username Username used to login into the database
+     *
+     * @return DateTime Return an object of {@link DateTime}.
      */
     public DateTime getTimestamp(String username) {
-        Log.d("LoginDBHelper", "getTimestamp");
-        SQLiteDatabase db = this.getReadableDatabase();
+        Log.d(this.getClass().getSimpleName(), "getTimestamp: getting the time stamp.");
+
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         DateTime entryTime = null;
-        Cursor cursor = db.rawQuery("select " + CURR_TIME + " from " +
-                                    contextObj.getString(R.string.tableLoginTrials) +
-                                    " where " + CUST_USERNAME + "='" + username + "'", null);
+        Cursor cursor = sqLiteDatabase.rawQuery("select " + CURR_TIME + " from " +
+                contextObj.getString(R.string.tableLoginTrials) + " where " + CUST_USERNAME
+                + "='" + username + "'", null);
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
             entryTime = format.withOffsetParsed().parseDateTime(cursor.getString(0));
         }
         if (cursor != null)
-            cursor.close();return entryTime;
+            cursor.close();
+        return entryTime;
     }
 
     /**
-     * isLocked is called to check if the account is locked due to 3 or more unsuccessful login attempts
+     * isLocked is a function that is called to check if the account is locked due
+     * to repeated unsuccessful login attempts.
      *
-     * @param username Username used to login into the database
+     * @param username Username used to login into the database.
+     *
+     * @return boolean  Return a boolean value depending if the account is locked.
      */
     public boolean isLocked(String username) {
-        SQLiteDatabase db = this.getReadableDatabase();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         boolean locked = false;
-        Cursor cursor = db.rawQuery("select " + isLocked + " from " +
-                                    contextObj.getString(R.string.tableLoginTrials) +
-                                    " where " + CUST_USERNAME + "='" + username + "'", null);
+        Cursor cursor = sqLiteDatabase.rawQuery("select " + isLocked + " from " +
+                contextObj.getString(R.string.tableLoginTrials) +
+                " where " + CUST_USERNAME + "='" + username + "'", null);
         if (cursor != null && cursor.getCount() > 0) {
             cursor.moveToFirst();
             locked = cursor.getInt(0) == 1;
@@ -138,14 +166,14 @@ public class LoginDBHelper extends DBHelper {
     }
 
     /**
-     * deleteTrial is called to delete the entry from table on successful login
+     * deleteTrial is a function that is called to delete the entry from table on successful login.
      *
      * @param username Username used to login into the database
      */
     public void deleteTrial(String username) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(contextObj.getString(R.string.tableLoginTrials),
-                    CUST_USERNAME + "='" + username + "'", null);
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        sqLiteDatabase.delete(contextObj.getString(R.string.tableLoginTrials),
+                CUST_USERNAME + "='" + username + "'", null);
     }
 
 }
